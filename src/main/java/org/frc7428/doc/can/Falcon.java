9f473c61ccb22ec4c3ac3f3d41a65fd4563845cd
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2022-09-28 21:21:01 -0500

    add example java docs

--- src/main/java/org/frc7428/doc/can/Falcon.java
+++ src/main/java/org/frc7428/doc/can/Falcon.java
@@ -2,13 +2,26 @@ package org.frc7428.doc.can;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+/**
+ * Custom wrapping for a TalonFX Motor Controller (Falcon 500)
+ */
 public class Falcon {
     private final WPI_TalonFX mTalon;
 
+    /**
+     * Creates a Falcon with the given CAN ID.
+     * 
+     * @param canId the CAN ID of the Falcon's TalonFX
+     */
     public Falcon(int canId) {
         mTalon = new WPI_TalonFX(canId);
     }
 
+    /**
+     * Sets the output of the motor.
+     * 
+     * @param scaledOutput motor's output between -1.0 and +1.0
+     */
     public void set(double scaledOutput) {
         mTalon.set(scaledOutput);
     }
--- src/main/java/org/frc7428/doc/can/Neo.java
+++ src/main/java/org/frc7428/doc/can/Neo.java
@@ -3,13 +3,26 @@ package org.frc7428.doc.can;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+/**
+ * Custom wrapping for a SPARK Max Motor Controller (Neo or Neo 550)
+ */
 public class Neo {
     private final CANSparkMax mNeo;
 
+    /**
+     * Creates a Neo/Neo 550 with the given CAN ID.
+     * 
+     * @param canId the CAN ID of the Neo's SPARK Max
+     */
     public Neo(int canId) {
         mNeo = new CANSparkMax(canId, MotorType.kBrushless);
     }
 
+    /**
+     * Sets the output of the motor.
+     * 
+     * @param scaledOutput motor's output between -1.0 and +1.0
+     */
     public void set(double scaledOutput) {
         mNeo.set(scaledOutput);
     }
